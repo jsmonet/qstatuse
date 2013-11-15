14377c5ce35a804c765a551ae58243e52ef72abf
Joe Franco <joe@ini.usc.edu>
2013-11-15 13:59:12 -0800

    Changed the structure to put the gathering into a function

--- use_by_users.sh
+++ use_by_users.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# This script returns the users and number of their jobs in descending order ranked by 
+# number of jobs. Your output should resemble
+# 39: username1
+# 25: username2
+# 1: username3
+# If it isn't acting like that, I blame you.
 
-USER_ARRAY=($(qstat | grep -v "^job" | grep -v "^\-" | awk '{print$4}' | sort | uniq ))
+function load_array(){
+XYZ=($(qstat | grep -v "^job" | grep -v "^\-" | awk '{print$4}' | sort -g | uniq ))
 
-for i in "${USER_ARRAY[@]}"
+for i in "${XYZ[@]}"
 do
-	sum=$( qstat | grep $i | wc -l )
-	echo "$i: $sum"
+        sum=$( qstat | grep $i | wc -l )
+        echo "$sum:$i" 
 done
+}
+
+load_array | sort -rg
+
+exit $?
